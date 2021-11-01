8ac30c13494b23a8a2c834c4b26e70febb7b0c47
JDobbe <jeroen.dobbe@student.rocva.nl>
2021-11-01 09:20:45 +0100

    eerste commit

---
+++ eindopdracht/eindopdracht.py
@@ -0,0 +1,14 @@
+#
+print("Hallo")
+name = input("Wat is jouw naam?")
+
+print("leuk om je te ontmoeten,", name)
+answer = input("hoe is het weer vandaag?")
+
+if answer == "zonnig":
+    print("ah, wat lekker zeg!")
+if answer == "regen"
+    print("jammer weer dit")
+if answer == "bewolkt"
+    print("oh dat is wel okay")
+else print("okay")
